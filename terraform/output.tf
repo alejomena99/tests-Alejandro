6e023ff2241b52a9a46677b905114c7b430f0b58
tests-Alejandro <manuel.mena@quind.io>
2024-03-05 13:42:41 -0500

    test

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,7 +2,7 @@ output "resources_location" {
   value = var.location
 }
 
-# output "virtual_machine" {
-#   value = resource.azurerm_linux_virtual_machine.virtual_machine
-# }
+output "virtual_machine_ip" {
+  value = resource.azurerm_linux_virtual_machine.virtual_machine.public_ip_address
+}
 
