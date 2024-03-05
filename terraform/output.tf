7b073b6ec09c29da072558098eb0b93ca1418f06
tests-Alejandro <manuel.mena@quind.io>
2024-03-05 13:12:08 -0500

    test

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,7 +2,7 @@ output "resources_location" {
   value = var.location
 }
 
-output "virtual_machine" {
-  value = resource.azurerm_linux_virtual_machine.virtual_machine
-}
+# output "virtual_machine" {
+#   value = resource.azurerm_linux_virtual_machine.virtual_machine
+# }
 
